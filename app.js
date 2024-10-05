4b1aa6ecdd354e8a7af3025ac47565a3ce0933ae
kasarlaraviteja7 <raviteja.kasarla7@gmail.com>
2024-10-05 21:25:14 +0530

    added app.js

---
+++ app.js
@@ -0,0 +1,11 @@
+const express = require('express'); // If you are using Express
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
